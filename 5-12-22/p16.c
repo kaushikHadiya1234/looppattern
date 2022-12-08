929dd951eeaff32577e1032cbbe5bb17c5414c71
kaushikHadiya1234 <119835333+kaushikHadiya1234@users.noreply.github.com>
2022-12-08 11:12:51 +0530

    Add files via upload

---
+++ 5-12-22/p16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int i,j,k;
+	
+	for(i='E';i>='A';i--)
+	{
+		for(k='E';k>=i;k--)
+		{
+			printf(" ");
+		}
+		for(j='A';j<=i;j++)
+		{
+			printf("%c",j);
+		}
+		printf("\n");
+	}
+}
